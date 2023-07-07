4132fbeafbb4cb5eddeae459225f264a121ff35e
theAndyMurray <murray0312@gmail.com>
2023-07-07 10:42:07 -0500

    starting indexeddb lab

--- js/main.js
+++ js/main.js
@@ -14,6 +14,32 @@
  limitations under the License.
  */
 
+// Register the service worker
+if ('serviceWorker' in navigator) {
+  // Wait for the 'load' event to not block other work
+  window.addEventListener('load', async () => {
+    // Try to register the service worker.
+    try {
+      // Capture the registration for later use, if needed
+      let reg;
+
+      // import.meta.env.DEV is a special environment variable injected by Vite to let us know we're in development mode. Here, we can use the JS Module form of our service worker because we can control our browsers in dev.
+      if (import.meta.env.DEV) {
+        reg = await navigator.serviceWorker.register('/service-worker.js', {
+          type: 'module',
+        });
+      } else {
+        // In production, we use the normal service worker registration
+        reg = await navigator.serviceWorker.register('/service-worker.js');
+      }
+
+      console.log('Service worker registered! 😎', reg);
+    } catch (err) {
+      console.log('😥 Service worker registration failed: ', err);
+    }
+  });
+}
+
 window.addEventListener('DOMContentLoaded', async () => {
   // Set up the editor
   const { Editor } = await import('./app/editor.js');
@@ -28,25 +54,3 @@ window.addEventListener('DOMContentLoaded', async () => {
 
   editor.setContent(defaultText);
 });
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', async () => {
-    try {
-      let reg;
-      // Use ES Module version of our Service Worker in development
-      if (import.meta.env?.DEV) {
-        reg = await navigator.serviceWorker.register('/service-worker.js', {
-          type: 'module',
-        });
-      } else {
-        // In production, use the normal service worker registration
-        reg = await navigator.serviceWorker.register('/service-worker.js');
-      }
-       
-      console.log('Service worker registration successful:', reg);
-    } catch (error) {
-      console.log('Service worker registration failed:', error);
-    }
-    
-  });
-}
--- service-worker.js
+++ service-worker.js
@@ -14,59 +14,46 @@ Copyright 2021 Google LLC
  limitations under the License.
  */
 
- import { warmStrategyCache, offlineFallback } from 'workbox-recipes';
- import { CacheFirst } from 'workbox-strategies';
- import { registerRoute } from 'workbox-routing';
- import { CacheableResponsePlugin } from 'workbox-cacheable-response';
- import { ExpirationPlugin } from 'workbox-expiration';
- 
- // Set up page cache
- const pageCache = new CacheFirst({
-   cacheName: 'page-cache',
-   plugins: [
-     new CacheableResponsePlugin({
-       statuses: [0, 200],
-     }),
-     new ExpirationPlugin({
-       maxAgeSeconds: 30 * 24 * 60 * 60,
-     }),
-   ],
- });
- 
- warmStrategyCache({
-   urls: ['/index.html', '/'],
-   strategy: pageCache,
- });
+import { offlineFallback, warmStrategyCache } from 'workbox-recipes';
+import { CacheFirst, StaleWhileRevalidate } from 'workbox-strategies';
+import { registerRoute } from 'workbox-routing';
+import { CacheableResponsePlugin } from 'workbox-cacheable-response';
+import { ExpirationPlugin } from 'workbox-expiration';
 
- // Set up offline fallback
-offlineFallback({
-     pageFallback: '/offline.html',
-});
- 
- registerRoute(({ request }) => request.mode === 'navigate', pageCache);
-
-// When there's an incoming fetch request, try and respond with a precached resource, otherwise fall back to the network
-self.addEventListener('fetch', (event) => {
-  console.log('Fetch intercepted for:', event.request.url);
-  event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      if (cachedResponse) {
-        return cachedResponse;
-      }
-      return fetch(event.request);
+// Set up page cache
+const pageCache = new CacheFirst({
+  cacheName: 'page-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
     }),
-  );
+    new ExpirationPlugin({
+      maxAgeSeconds: 30 * 24 * 60 * 60,
+    }),
+  ],
 });
+
+warmStrategyCache({
+  urls: ['/index.html', '/'],
+  strategy: pageCache,
+});
+
+registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // Set up asset cache
 registerRoute(
-     ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
-     new StaleWhileRevalidate({
-          cacheName: 'asset-cache',
-          plugins: [
-               new CacheableResponsePlugin({
-                    statuses: [0, 200],
-               }),
-          ],
-     }),
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  }),
 );
+
+// Set up offline fallback
+offlineFallback({
+  pageFallback: '/offline.html',
+});
